14cd6506285e9b13b972511ee019addc90f91ed5
Diego Soriano <diegosorianomagallon@gmail.com>
2025-04-08 13:14:42 +0200

    v.0.1.0 08.04.2025 13.14 tabs

---
+++ front/framing-app/components/HapticTab.tsx
@@ -0,0 +1,20 @@
+import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
+import { PlatformPressable } from '@react-navigation/elements';
+import * as Haptics from 'expo-haptics';
+import { Platform } from 'react-native';
+
+export function HapticTab(props: BottomTabBarButtonProps) {
+  return (
+    <PlatformPressable
+      {...props}
+      android_ripple = {Platform.OS === 'android' ? { color: 'transparent', borderless: false}: undefined}
+      onPressIn={(ev) => {
+        if (process.env.EXPO_OS === 'ios') {
+          // Add a soft haptic feedback when pressing down on the tabs.
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        }
+        props.onPressIn?.(ev);
+      }}
+    />
+  );
+}
